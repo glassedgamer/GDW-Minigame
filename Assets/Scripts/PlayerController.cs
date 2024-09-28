990eab22c14b3e446c230efb20b04a774db8d941
glassedgamer <glassedgamer05@gmail.com>
2024-09-28 15:59:46 -0400

    Game
    Gave a car a gun

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    float horizontalInput;
+    float verticalInput;
+
+    public float speed = 20f;
+    public float turnSpeed = 40f;
+
+    public Transform shootingPoint;
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * verticalInput * speed * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, shootingPoint.position, transform.rotation);
+        }
+    }
+}
